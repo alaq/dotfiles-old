67bb39042b6513a8789ea3943dc92e74ecfcb6a4
Adrien <verblodung@gmail.com>
2018-10-24 23:56:32 -0400

    emacs custom config added

--- emacs.d/elisp/base-theme.el
+++ emacs.d/elisp/base-theme.el
@@ -1,6 +1,8 @@
-(use-package solarized-theme
+(use-package doom-themes
   :defer t
   :init
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
   (load-theme 'solarized-dark t))
 
 (provide 'base-theme)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,3 +15,69 @@
 (require 'lang-javascript)
 
 (require 'lang-web)
+
+;; original configuration below
+;; remove ui details
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+(setq initial-scratch-message "")
+;; (global-linum-mode 1)
+(global-hl-line-mode 1)
+(global-visual-line-mode 1)         ; line warp
+(setq inhibit-startu-screen t)
+(setq-default indent-tabs-mode nil) ; replace tabs with spaced
+(setq-default tab-width 2)
+
+;; package config
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org" . "http://orgmode.org/elpa")
+                         ("gnu" . "https://elpa.gnu.org/packages")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; evil mode
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode)
+
+;; this should replace the above
+;; ;; load evil
+;; (use-package evil
+;;   :ensure t ;; install the evil package if not installed
+;;   :init ;; tweak evil's configuration before loading it
+;;   (setq evil-search-module 'evil-search)
+;;   (setq evil-ex-complete-emacs-commands nil)
+;;   (setq evil-vsplit-window-right t)
+;;   (setq evil-split-window-below t)
+;;   (setq evil-shift-round nil)
+;;   (setq evil-want-C-u-scroll t)
+;;   :config ;; tweak evil after loading it
+;;   (evil-mode)
+
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+(require 'evil-commentary)
+(evil-commentary-mode)
+
+(require 'evil-numbers)
+(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook (lambda () (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+ 
